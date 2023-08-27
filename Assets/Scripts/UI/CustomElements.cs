60dcbe91b8567df108f9e37c7c91b38dcf358500
tmoosting <40519405+tmoosting@users.noreply.github.com>
2023-08-27 17:36:27 +0200

    etc

--- Assets/Scripts/UI/CustomElements.cs
+++ Assets/Scripts/UI/CustomElements.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+
+#if UNITY_EDITOR
 using Unity.EditorCoroutines.Editor;
+#endif
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -31,38 +34,4 @@ public class BlueTextField : TextField
     } 
 }
     
-    
-public class FrontLabel : VisualElement
-{
-    private ControlUI controlUI;
-    public Label label;
-         
-    public FrontLabel(ControlUI controlUI, bool anchorRight ,string textValue, VisualElement secondElement)
-    {
-        this.controlUI = controlUI;
-
-        secondElement.style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
-        style.flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Row);
-        if (anchorRight)
-            style.justifyContent = Justify.SpaceBetween;
-        else
-            style.justifyContent = Justify.FlexStart;
-
-        style.alignItems = Align.Center;
-    
-        label = new Label(textValue);
-        label.style.unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold);
-        this.Add(label); 
-        EditorCoroutineUtility.StartCoroutineOwnerless(SetMarginAfterDelay(secondElement));
-        
-        this.Add(secondElement);
-    }
-    private IEnumerator SetMarginAfterDelay(VisualElement secondElement)
-    {
-        yield return new EditorWaitForSeconds(0.01f); 
-        float margin = 80 - label.resolvedStyle.width;
-        secondElement.style.marginLeft =  margin;
-        secondElement.style.visibility = new StyleEnum<Visibility>(Visibility.Visible);
-    }
-    
-}
+     
